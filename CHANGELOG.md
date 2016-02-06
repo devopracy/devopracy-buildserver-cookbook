974ac58cd36b5e754ebdfb9a922b30c6c521ada9
ele munjeli <munjeli@localhost.localdomain>
2016-02-06 14:16:58 -0800

    cookbook init

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Devopracy Buildserver Cookbook CHANGELOG
+========================================
+
+v0.0.1 (2016-02-06)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# Encoding: UTF-8
+#
+
+name             'devopracy-buildserver'
+maintainer       'Ele Munjeli'
+maintainer_email 'munjeli@devopracy.net'
+license          'apache2'
+description      'Installs/Configures devopracy-buildserver'
+long_description 'Installs/Configures devopracy-buildserver'
+version          '0.0.1'
